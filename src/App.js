4dd1cb060c760e4f7bff918f374d39bc0ad5001b
Sanju Joseph <sanjuantony@gmail.com>
2021-01-06 22:50:33 -0500

    Bug fix 1 - 06Jan2021

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
